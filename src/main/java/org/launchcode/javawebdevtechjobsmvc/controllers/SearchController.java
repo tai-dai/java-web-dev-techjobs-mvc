6f30b51f3d130399280c6c06005fd1ffbee0fd3d
Tai Smith <imadeanomelet@gmail.com>
2021-01-21 10:44:20 -0600

    Completed search functionality and search-type memory.

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -19,10 +20,24 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
+        String typeSelection = "all";
+        model.addAttribute("typeSelection", typeSelection);
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+        String typeSelection = searchType;
+        model.addAttribute("columns", columnChoices);
+        if(searchType.toLowerCase(Locale.ROOT) == "all" || searchTerm.toLowerCase(Locale.ROOT) == "all"){
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("typeSelection", typeSelection);
+        return "search";
+    }
 }
